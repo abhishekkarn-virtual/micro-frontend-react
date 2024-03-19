f9548e1bf29401ddee201a02e72ad1d7f43c9a89
Abhishek Karan <abhishekaran.work@gmail.com>
2024-03-19 16:07:33 +0530

    initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,11 @@
+import { MarketingApp } from "./components/MarketingApp";
+import React from "react";
+
+export const App = () => {
+  return (
+    <div>
+      <h1>HI there!!</h1>
+      <MarketingApp />
+    </div>
+  );
+};
